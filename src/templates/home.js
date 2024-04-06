1390294792882e1015cb38b6701c58602b5fa6f4
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-04-07 03:25:50 +0330

    feat : create example home

---
+++ src/templates/home.js
@@ -0,0 +1,11 @@
+import { El } from "../utils/create-element";
+
+export function home() {
+  // inputPassword.addEventListener()
+  return El({
+    element: "a",
+    className: "",
+    href: "/login",
+    innerText: "gnfgn",
+  });
+}
