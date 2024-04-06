222a47149f806d4ab2e53c5aa7435a7708443de7
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-04-07 03:25:00 +0330

    feat: router home & loginform

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,11 @@
 import Navigo from 'navigo';
 import { changePage } from '../utils/change-page';
 import { loginForm } from '../templates/login';
+import { home } from '../templates/home';
 export const router = new Navigo('/');
 
 router.on('/', () => {
+  changePage(home);
+}).on('/login', () => {
   changePage(loginForm);
 });
