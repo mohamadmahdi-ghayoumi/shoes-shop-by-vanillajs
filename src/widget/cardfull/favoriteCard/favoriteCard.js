9434a33fc1f3a961e377fbbf8b9c0f21c9abeb44
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-04-16 20:40:50 +0330

    style: color icon like

--- src/widget/cardfull/favoriteCard/favoriteCard.js
+++ src/widget/cardfull/favoriteCard/favoriteCard.js
@@ -1,15 +1,24 @@
-import {
-  addToWishlist,
-} from "../../../api/post/postProduct";
+import { getWishlistProduct } from "../../../api/get/getProducts";
+import { addToWishlist } from "../../../api/post/postProduct";
 import { El } from "../../../utils/create-element";
 
 export function favoriteCard({ product }) {
+  
+  getWishlistProduct("all").then((data) => {
+    const findItemCart = data.find((item) => item.id == product.id);
+    if(findItemCart){
+      iconHeart.classList.remove("icon-[ph--heart-light]");
+      iconHeart.classList.add("icon-[icon-park-solid--like]");
+      iconHeart.classList.add("text-red-500");
+    }
+  });
+
   function sendToWishlist() {
     const iconHeart = document.getElementById("iconHeart");
     iconHeart.classList.remove("icon-[ph--heart-light]");
     iconHeart.classList.add("icon-[icon-park-solid--like]");
     iconHeart.classList.add("text-red-500");
-    addToWishlist(product)
+    addToWishlist(product);
   }
   return El({
     element: "div",
